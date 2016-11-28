1f46a229afc7901f9dda7f81e12d60696eb95627
John KM Jung <min85da@gmail.com>
2016-11-27 21:38:01 -0500

    deploying to heroku

--- server/db/db.js
+++ server/db/db.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 var mongodb = require('mongodb');
 
 //Legacy group: you're welcome to use our developer version of the
-//database we set up here in the second part of the "OR" logic. 
+//database we set up here in the second part of the "OR" logic.
 //May be best though to replace the hard-coded developer database with your own
 //b/c you will not be able to access our database in our heroku account to view it
-var dbUrl = process.env.MONGOURI || 'mongodb://heroku_s13dqq10:c5b7hlbujuecnetp4mppna4p43@ds057386.mlab.com:57386/heroku_s13dqq10'
+var dbUrl = process.env.MONGODB_URI
 
 mongoose.connect(dbUrl, function(err, res) {
   if (err) console.error('ERROR connecting to: ' + dbUrl + '. ' + err)
